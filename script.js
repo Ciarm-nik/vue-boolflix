3757be08c3e866c439fa89dfc05959568b959c19
Ciarm-nik <nikcia@libero.it>
2021-05-13 19:43:25 +0200

    Milestone 1

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-new Vue ({
-    el: "#app",
+new Vue({
+    el: '#app',
     data: {
+        films: [],
+        query: ''
 
     },
+
     methods: {
+        utent_search(query) {
 
-    },
-    computed: {
-
-    },
-    mounted (){
-
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: '513afbf57079cfde7b003498cfb4db65',
+                    query: query,
+                    language: "it-IT"
+                }
+            })
+                .then((res) => {
+                    this.films = res.data.results;
+                })
+            this.query = '';
+        }
     }
 })
