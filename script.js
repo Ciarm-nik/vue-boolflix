63686194f135f78679ed895d6d11f9abfbc87a9f
Ciarm-nik <nikcia@libero.it>
2021-05-17 16:12:31 +0200

    Stampo in console i primi 5 attori

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ new Vue({
         media: [],
         query: '',
         solid: "fas",
-        image: null
+        image: null,
+        ricerca: false,
+        cast: []
     },
     methods: {
         utent_search(query) {
+            // this.ricerca = true
             // Cambio l'url in search/multi per cercare anche le serie tv oltre ai film (modifico il ciclo for nell'html)
             axios.get('https://api.themoviedb.org/3/search/multi', {
                 params: {
@@ -18,8 +21,9 @@ new Vue({
             })
                 .then((res) => {
                     this.media = res.data.results;
+                    // this.ricerca= false;
                 })
-            this.query = '';
+            // this.query = '';
         },
         // getImgSrc (media) {
         //     if(media.poster_path) {
@@ -28,6 +32,21 @@ new Vue({
         //         return "../img/not_found.jpg"
         //     }
         // }
+        cast_search(id, media_type){
+
+            axios.get('https://api.themoviedb.org/3/' + media_type + '/' + id + '/credits',{
+                params: {
+                    api_key: '513afbf57079cfde7b003498cfb4db65',
+                }
+            })
+            .then((res) => {
+                // return res.data.cast.slice(0, 5);
+                this.cast = res.data.cast.slice(0, 5);
+                this.cast.forEach ((item) => {
+                    console.log(item.name)
+                });
+            })
+        },
     },
     computed: {
 
